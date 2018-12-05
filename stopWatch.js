22d154ea1418d0b44cfabb99c66725d0ca58e6bc
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-05 14:49:42 +0300

    Start/Stop, Record timer, Reset Timer

--- stopWatch.js
+++ stopWatch.js
@@ -1,17 +1,53 @@
 // global variables
-var start = document.getElementById("startStop");
-var startWatch = true;
-var stopWatch = true;
+var time = 0;
 
-start.addEventListener("click", function() {
-	toggle();
-});
+var start = document.getElementById('startStop');
+var displayTime = document.getElementById('timesection');
+var resetTimer = document.getElementById('resetClock');
+var recordTimer = document.getElementById('recordTime');
+var recordList = document.getElementById('recordedtimesection');
+
+var startWatch = true;
+var timer;
+var captureTime;
+
+setUp();
+
+function startTimer() {
+	timer = setInterval(function() {
+		time++;
+		displayTime.innerHTML = time / 100;
+	});
+}
+
+function setUp() {
+	displayTime.innerHTML = time;
+	start.addEventListener('click', function() {
+		toggle();
+	});
+
+	resetTimer.addEventListener('click', function() {
+		clearInterval(timer);
+		startWatch = true;
+		displayTime.innerHTML ='0';
+		recordList.innerHTML ='';
+		start.innerHTML = 'Start';
+	});
+
+	recordTimer.addEventListener('click', function() {
+		captureTime = time / 100;
+		recordList.innerHTML += captureTime + '<br />';
+	});
+}
 
 function toggle() {
 	if (startWatch) {
-		start.innerHTML = "Stop";
-			startWatch = false;
+		start.innerHTML = 'Stop';
+		startTimer();
+		startWatch = false;
 	} else {
-		start.innerHTML = "Start";
+		start.innerHTML = 'Start';
+		clearInterval(timer);
+		startWatch = true;
 	}
 }
