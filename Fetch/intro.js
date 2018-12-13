2c485bba23fd98e89b2ac99cd21d31b57bbb0d09
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-13 14:41:45 +0300

    Defining Fetch API - extracting data :smirk:

--- Fetch/intro.js
+++ Fetch/intro.js
@@ -2,12 +2,26 @@
 // used instead of XMLHttp request
 //method returns a Promise that contains a Response object
 // returns a promise
+// The fetch() method takes in an URL endpoint and is used to make a network request
+// extraction methods
+
+// .json()
+// .text()
+// .blob()
 
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 fetch("https://jsonplaceholder.typicode.com/todos/1").then(function(result) {
 	return result.json();
+}).then(function(result) {
+	console.log(result);
+}).catch(function(error) {
+	console.log(error);
+});
+
+fetch("https://jsonplaceholder.typicode.com/todos/1").then(function(result) {
+	return result.text();
 }).then(function(result) {
 	console.log(result);
 }).catch(function(error) {
