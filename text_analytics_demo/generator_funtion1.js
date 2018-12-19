01f979c25549eab4699c0403d3a4d2486fcc19bc
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-12-19 12:27:32 +0300

    Creating and Iterating through a Generator Object

--- text_analytics_demo/generator_funtion1.js
+++ text_analytics_demo/generator_funtion1.js
@@ -1,12 +1,16 @@
 // Generators are functions that can be paused and be resumd
 // yeild to pause and return the the output or receive the output
 function* genFunc() {
+	console.log("started");
 	yield 'a';
+	console.log("passed first yield");
 	yield 'Daniel Mutwiri';
+	console.log("passed second yield");
 	yield ;
+	console.log("Passed third yield");
 	yield 123;
 
 	return "finished";
 }
 
-genFunc();
+var genObject = genFunc();
